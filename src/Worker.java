a461477506403341a65bf7f698efa65eba4dc375
Jearpo <jesusarizajrj@gmail.com>
2024-06-07 21:05:17 +0200

    Worket setup

--- src/Worker.java
+++ src/Worker.java
@@ -1,4 +1,23 @@
+import java.util.List;
 
-public class Worker {
+import javax.swing.SwingWorker;
 
+public class Worker extends SwingWorker<List<Primos>, Void>{
+
+	private int workerNumber;
+	private Panel panel;
+	
+	public Worker(int wn, Panel p)
+	{
+		workerNumber = wn;
+		panel = p;
+	}
+	
+	@Override
+	protected List<Primos> doInBackground() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
