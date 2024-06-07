3d0e1ce3cad7b87b49264f063599500b65353b88
Jearpo <jesusarizajrj@gmail.com>
2024-06-07 20:18:49 +0200

    Primos.java implemented

--- src/Primos.java
+++ src/Primos.java
@@ -1,4 +1,23 @@
 
 public class Primos {
-
+	private int pri1;
+	private int pri2;
+	private int pos;
+	
+	public Primos(int p1, int p2, int p)
+	{
+		pri1 = p1;
+		pri2 = p2;
+		p = pos;
+	}
+	
+	public String toString()
+	{
+		return "(" + pri1 + ", " + pri2 + ")";
+	}
+	
+	public int getPos()
+	{
+		return pos;
+	}
 }
