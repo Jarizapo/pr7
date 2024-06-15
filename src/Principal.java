4ba6f29ad2e1f77eb885535f69dc8f1351bb13ff
Jearpo <jesusarizajrj@gmail.com>
2024-06-15 23:19:50 +0200

    Principal.java implemented as class' exercises (Controlador not created)

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,39 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Principal {
+	
+	public static void crearGUI()
+	{
+       	System.out.println("crearGUI() - isEventDispatchThread? "+ SwingUtilities.isEventDispatchThread());
 
+        JFrame ventana = new JFrame("Numeros Primos");
+        
+        Panel panel = new Panel();
+
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.getContentPane().add(panel);
+        ventana.pack();
+        ventana.setVisible(true);
+	}
+	
+    public static void main(String[] args)
+    {
+
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+            	try
+            	{
+            		crearGUI();
+            	}
+            	catch(Exception e)
+            	{
+            		System.out.println("Tarea cancelada");
+            	}
+            }
+        });
+    }
 }
